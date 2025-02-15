5f3faba3f1af7e63bdb8d54233d1e974d8bcaffe
Rafatuba <rafaolly1@gmail.com>
2025-02-15 13:10:12 -0400

    Implementação do componente ScrollTop e link para detalhes do produto.

--- src/components/card-produto/index.tsx
+++ src/components/card-produto/index.tsx
@@ -1,14 +1,18 @@
+import { NavLink } from "react-router-dom";
+
 export default function CardProduto() {
   return (
-    <div className="flex flex-col justify-center items-center gap-4 h-max shadow-lg p-7 bg-white rounded-lg">
-      <h1 className="text-center font-bold text-xl">Echo Spot com Alexa</h1>
-      <img
-        src="https://m.media-amazon.com/images/I/71anQJA7UcL._AC_SL1500_.jpg"
-        alt="imagem do produto"
-        className="w-40"
-      />
-      <p className="w-full font-bold text-lg">Amazon</p>
-      <p className="w-full">R$ 459,90</p>
-    </div>
+    <NavLink to="/detalhes">
+      <div className="flex flex-col justify-center items-center gap-4 h-max shadow-lg p-7 bg-white rounded-lg">
+        <h1 className="text-center font-bold text-xl">Echo Spot com Alexa</h1>
+        <img
+          src="https://m.media-amazon.com/images/I/71anQJA7UcL._AC_SL1500_.jpg"
+          alt="imagem do produto"
+          className="w-40"
+        />
+        <p className="w-full font-bold text-lg">Amazon</p>
+        <p className="w-full">R$ 459,90</p>
+      </div>
+    </NavLink>
   );
 }
--- src/templates/userTemplate/index.tsx
+++ src/templates/userTemplate/index.tsx
@@ -1,9 +1,11 @@
 import Footer from "../../components/footer";
 import Header from "../../components/header";
+import ScrollTop from "../../components/scrollTop";
 
 export default function UserTemplate(props: { children: React.ReactNode }) {
   return (
     <div className="h-screen flex flex-col relative">
+      <ScrollTop />
       <Header />
       <div className="flex flex-1 flex-col px-[5%] mt-20 max-w-[1920px]">
         {props.children}
