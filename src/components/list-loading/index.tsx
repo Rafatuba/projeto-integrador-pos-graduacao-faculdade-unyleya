18f0de3830693efa9c615bec62b32ae1d4b1e6d9
Rafatuba <rafaolly1@gmail.com>
2025-02-27 12:07:37 -0400

    Ajustes no skeleton e exclusão de arquivo

--- src/components/list-loading/index.tsx
+++ src/components/list-loading/index.tsx
@@ -4,7 +4,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 export default function ListLoading() {
   return (
     <div className="flex flex-wrap gap-4">
-      {Array.from({ length: 6 }).map((item, index) => (
+      {Array.from({ length: 6 }).map((_item, index) => (
         <div
           key={index}
           className="flex flex-col justify-around items-center gap-4 w-3xs h-96 shadow-lg p-7 bg-white rounded-lg"
